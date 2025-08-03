b5d27ba1cfc449911a8aab0591a1b0de865ed924
Rafael Andrade <rafael.santos.andrade@hotmail.com>
2025-08-03 18:07:58 -0300

    feat: add custom logger

--- main.go
+++ main.go
@@ -1,8 +1,22 @@
 package main
 
-import "github.com/rafael-andrade-se/gopportunities/router"
+import (
+	"github.com/rafael-andrade-se/gopportunities/config"
+	"github.com/rafael-andrade-se/gopportunities/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+	logger = config.GetLogger("main")
+	// Initialize Configs
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("Config initialization error: %v", err)
+		return
+	}
 
 	// Initialize Router
 	router.Initialize()
